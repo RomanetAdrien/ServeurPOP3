006c01eba5192409f4be6f59f05bc49629bafde0
Adrien Romanet <Adrien Romanet>
2017-03-20 09:11:50 +0100

    Client fait ?

--- src/Client/ClientConnexion.java
+++ src/Client/ClientConnexion.java
@@ -1,11 +1,11 @@
 package Client;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import javafx.scene.control.TextInputDialog;
+
+import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.Scanner;
 
 /**
  * Created by p1206717 on 06/03/2017.
@@ -29,12 +29,76 @@ public class ClientConnexion {
         String answer = null;
         answer = in.readLine();
 
-        System.out.println(answer);
+        if(!answer.equals("+OK Server ready")){
+            System.out.println("Could not connect to Server");
+            return;
+        }
 
-        OutputStream out = null;
+        String message="";
+        do {
+            OutputStream out = socket.getOutputStream();
+            Scanner userinput = new Scanner(System.in);
+            System.out.print("Identify yourself for the server :");
+            String identification = userinput.next();
+            System.out.print(identification);
+
+            message = "APOP " + identification +"\r\n";
+            out.write(message.getBytes());
+            InputStream input = socket.getInputStream();
+            message = listen(input);
+            System.out.println(message);
+
+        }while (!message.startsWith("+OK"));
+
+        do {
+            OutputStream out = socket.getOutputStream();
+            Scanner userinput = new Scanner(System.in);
+            System.out.print("Write a command for the server :");
+            String command = userinput.next();
+            System.out.print(command);
+
+            message = "";
+            out.write(message.getBytes());
+            InputStream input = socket.getInputStream();
+            message = listen(input);
+            System.out.println(message);
+
+        }while (!message.equals("QUIT"));
+
+
+
+
+
 
     }
 
+    private  String listen(InputStream input) throws IOException {
+        boolean cr = false;
+        boolean lf = false;
+        String message = "";
+
+        while(!cr || !lf){
+            int data = input.read();
+            if(data == -1){
+                return "quitnonsafe";
+            }
+            char c = (char)data;
+            message += c;
+
+            if(cr && c == '\n')
+                lf = true;
+            else
+                cr = false;
+            if(c == '\r')
+                cr = true;
+        }
+        if(message.equalsIgnoreCase("quitnonsafe")){
+            message += ".";
+        }
+        message = message.replaceAll("[\r,\n]", "");
+        return message;
+    }
 }
+
 
 
--- src/Client/ClientPOP3.java
+++ src/Client/ClientPOP3.java
@@ -1,11 +1,15 @@
 package Client;
 
+import javafx.application.Application;
+import javafx.scene.control.TextInputDialog;
+
 import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * Created by p1206717 on 06/03/2017.
  */
-public class ClientPOP3 {
+public class ClientPOP3{
 
     public static void main(String[] args) throws IOException {
 
